22cf9ec711348934c4e5378a671f784caf7de2fd
Mudit Choudhary <cmudit60@gmail.com>
2022-06-18 18:27:16 +0530

    Added a function to add marker by players

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
 const gameBoard = (() => {
-    const array = () => ['x','o','x','o','x','o','x','o','x'];
-    return {array}
+  const array = ["x", "o", "x", "o", "x", "o", "x", "o", "x"];
+  return { array };
 })();
 
 const player = (name, marker) => {
-    const getName = () => name;
-    const getMarker = () => marker;
+  const boxes = document.querySelectorAll(".box");
+  const getName = () => name;
+  const getMarker = () => marker;
 
-    const addMarker = (box) => {
-        box.innerText = 'X';
-    }
-    return {getName, getMarker, addMarker}
-}
+  const addMarker = (box, marker) => {
+    box.innerText = marker;
+  };
+
+  return { getName, getMarker, addMarker };
+};
 
 const game = (() => {
-    const boxes = document.querySelectorAll('.box');
-    const playerOne = player('Mudit', 'O');
+  const playerOne = player("Mudit", "X");
+  const playerTwo = player("Rohan", "O");
+  const boxes = document.querySelectorAll(".box");
+  const defaultPlayer = playerOne;
+  let currentPlayer = defaultPlayer;
 
-    const renderMarker = () => {
-        const array = gameBoard.array();
-        let i = 0;
-        for (box of boxes) {
-            box.innerText = array[i];
-            i++;
-        }
+  const renderMarker = () => {
+    const array = gameBoard.array;
+    let i = 0;
+    for (box of boxes) {
+      box.innerText = array[i];
+      i++;
     }
+  };
 
-    const addClickEventOnBox = () => {
-        boxes.forEach(box => {
-            box.addEventListener('click', (e) =>{
-               playerOne.addMarker(e.target);
-            });
-        });
+  const addClickEventOnBox = () => {
+    boxes.forEach((box) => {
+      box.addEventListener("click", (e) => {
+        const player = checkWhichPlayer();
+        player.addMarker(e.target, player.getMarker());
+      });
+    });
+  };
+
+  const checkWhichPlayer = () => {
+    if (currentPlayer === playerOne) {
+      currentPlayer = playerTwo;
+      return playerOne;
+    } else {
+      currentPlayer = playerOne;
+      return playerTwo;
     }
-    return {renderMarker, addClickEventOnBox};
+  };
+  return { renderMarker, addClickEventOnBox };
 })();
