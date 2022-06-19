3c42a9d4a7d2ba7f2372bb1925d7e4ed1e81e013
Mudit Choudhary <cmudit60@gmail.com>
2022-06-19 21:10:35 +0530

    Add function to check for win and tie and over the game

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameBoard = (() => {
-  const array = ["x", "o", "x", "o", "x", "o", "x", "o", "x"];
+  const array = ["", "", "", "", "", "", "", "", ""];
   return { array };
 })();
 
@@ -21,31 +21,38 @@ const game = (() => {
   const boxes = document.querySelectorAll(".box");
   const defaultPlayer = playerOne;
   let currentPlayer = defaultPlayer;
+  const array = gameBoard.array;
+  let lengthCounter = 0;
 
   const renderMarker = () => {
-    const array = gameBoard.array;
     let i = 0;
     for (box of boxes) {
       box.innerText = array[i];
-      box.setAttribute('data-isclick', 'not-clicked');
+      box.setAttribute("data-isclick", "not-clicked");
+      box.setAttribute("data-index", `${i}`);
       i++;
+    }
+  };
+
+  const anynamousFunctionHandler = (e) => {
+    if (e.target.getAttribute("data-isclick") === "not-clicked") {
+      const player = switchPlayer();
+      player.addMarker(e.target, player.getMarker());
+      const boxIndex = e.target.getAttribute("data-index");
+      gameBoard.array[boxIndex] = player.getMarker();
+      e.target.setAttribute("data-isclick", "clicked");
+      lengthCounter++;
+      checkGameOver();
     }
   };
 
   const addClickEventOnBox = () => {
     boxes.forEach((box) => {
-      box.addEventListener("click", (e) => {
-        console.log(e.target.getAttribute('data-isclick'));
-        if (e.target.getAttribute('data-isclick') === 'not-clicked') {
-            const player = checkWhichPlayer();
-            player.addMarker(e.target, player.getMarker());
-            e.target.setAttribute('data-isclick', 'clicked');
-        }
-      });
+      box.addEventListener("click", anynamousFunctionHandler);
     });
   };
 
-  const checkWhichPlayer = () => {
+  const switchPlayer = () => {
     if (currentPlayer === playerOne) {
       currentPlayer = playerTwo;
       return playerOne;
@@ -55,8 +62,84 @@ const game = (() => {
     }
   };
 
-  const removeClickEventFromBox = (box) => {
-    box.removeEventListener("click", () => {});
+  const checkGameOver = () => {
+    if (
+      (array[0] === "X" && array[1] === "X" && array[2] === "X") ||
+      (array[0] === "O" && array[1] === "O" && array[2] === "O")
+    ) {
+      console.log("win");
+      gameOver();
+    }
+    if (
+      (array[3] === "X" && array[4] === "X" && array[5] === "X") ||
+      (array[3] === "O" && array[4] === "O" && array[5] === "O")
+    ) {
+      console.log("win");
+      gameOver();
+    }
+    if (
+      (array[6] === "X" && array[7] === "X" && array[8] === "X") ||
+      (array[6] === "O" && array[7] === "O" && array[8] === "O")
+    ) {
+      console.log("win");
+      gameOver();
+    }
+    if (
+      (array[0] === "X" && array[3] === "X" && array[6] === "X") ||
+      (array[0] === "O" && array[3] === "O" && array[6] === "O")
+    ) {
+      console.log("win");
+      gameOver();
+    }
+    if (
+      (array[1] === "X" && array[4] === "X" && array[7] === "X") ||
+      (array[1] === "O" && array[4] === "O" && array[7] === "O")
+    ) {
+      console.log("win");
+      gameOver();
+    }
+    if (
+      (array[2] === "X" && array[5] === "X" && array[8] === "X") ||
+      (array[2] === "O" && array[5] === "O" && array[8] === "O")
+    ) {
+      console.log("win");
+      gameOver();
+    }
+    if (
+      (array[0] === "X" && array[4] === "X" && array[8] === "X") ||
+      (array[0] === "O" && array[4] === "O" && array[8] === "O")
+    ) {
+      console.log("win");
+      gameOver();
+    }
+    if (
+      (array[2] === "X" && array[4] === "X" && array[6] === "X") ||
+      (array[2] === "O" && array[4] === "O" && array[6] === "O")
+    ) {
+      console.log("win");
+      gameOver();
+    }
+    if (checkArrayFilled()) {
+      console.log("Tie");
+      gameOver();
+    }
+  };
+
+  const checkArrayFilled = () => {
+    if (lengthCounter === gameBoard.array.length) {
+      return true;
+    }
+    return false;
+  };
+
+  const gameOver = () => {
+    removeClickEventOnBox();
+  };
+
+  const removeClickEventOnBox = () => {
+    boxes.forEach((box) => {
+      box.removeEventListener("click", anynamousFunctionHandler);
+    });
   };
   return { renderMarker, addClickEventOnBox };
 })();
